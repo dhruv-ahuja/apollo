101851341161c96ab1731b3fa933291c808f25cd
Dhruv <83733638+dhruv-ahuja@users.noreply.github.com>
2023-06-10 13:12:59 +0530

    fix: potential theme issues on some browsers - load dark.css file separately when toggle state is set - fix themes not loading when theme state is not `toggle`

--- static/js/themetoggle.js
+++ static/js/themetoggle.js
@@ -1,17 +1,22 @@
-function setTheme(mode) {
+function setTheme(mode, toggleButtonExists) {
     localStorage.setItem("theme-storage", mode);
     let htmlElement = document.querySelector("html");
 
     if (mode === "dark") {
-        document.getElementById("darkModeStyle").disabled = false;
-        document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"sun\"></i>";
-        feather.replace()
+        if (toggleButtonExists === true) {
+            document.getElementById("darkModeStyle").disabled = false;
+            document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"sun\"></i>";
+            feather.replace()
+        }
         htmlElement.classList.add("dark")
         htmlElement.classList.remove("light")
+
     } else if (mode === "light") {
-        document.getElementById("darkModeStyle").disabled = true;
-        document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"moon\"></i>";
-        feather.replace()
+        if (toggleButtonExists === true) {
+            document.getElementById("darkModeStyle").disabled = true;
+            document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"moon\"></i>";
+            feather.replace()
+        }
         htmlElement.classList.add("light")
         htmlElement.classList.remove("dark")
     }
@@ -25,5 +30,15 @@ function toggleTheme() {
     }
 }
 
-var savedTheme = localStorage.getItem("theme-storage") || "light";
-setTheme(savedTheme);
+// * run only if toggle mode is set 
+var toggleButton = document.getElementById("dark-mode-toggle")
+
+if (toggleButton !== null) {
+    let savedTheme = localStorage.getItem("theme-storage") || "light";
+    setTheme(savedTheme, true)
+}
+
+// * set theme based on current selection in config.toml `extra` section
+function manualSetTheme(mode) {
+    setTheme(mode, false)
+}
